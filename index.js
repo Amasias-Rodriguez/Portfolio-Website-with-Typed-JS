fbc6b93f3071d964999db61b785d2939859ea5ce
Amasías Rodríguez <100006082+Amasias-Rodriguez@users.noreply.github.com>
2023-03-17 00:11:09 -0600

    javascript file

---
+++ index.js
@@ -0,0 +1,13 @@
+let typingText = new Typed("#text",{
+    strings: ["John", "Designer", "Coder", "Youtuber"],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+})
+
+let banner = document.querySelector('.banner');
+let dayNight = document.querySelector('.dayNight');
+dayNight.onclick = function(){
+    banner.classList.toggle('night')
+}
